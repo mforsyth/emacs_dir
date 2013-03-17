543c9c61572ca9305a44c4c2517f763a0f9646aa
David Leal <dgleal@gmail.com>
2013-03-17 16:48:59 +0000

    Add initial clojure config.

---
+++ init.d/500-init-clojure.el
@@ -0,0 +1,14 @@
+(require 'nrepl)
+
+(setq nrepl-hide-special-buffers t)
+(setq nrepl-popup-stacktraces nil)
+(setq nrepl-popup-stacktraces-in-repl t)
+
+(add-hook 'clojure-mode-hook (lambda () (autopair-mode -1)))
+(add-hook 'clojure-mode-hook 'paredit-mode)
+
+(add-hook 'nrepl-mode-hook (lambda () (autopair-mode -1)))
+; (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+(add-hook 'nrepl-mode-hook 'subword-mode)
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
