aa47806b02ee1de4f4442020e0f813efb39eadd3
David Leal <dgleal@gmail.com>
2013-03-24 18:50:59 +0000

    Add $EDITOR.

--- init.d/000-init-env.el
+++ init.d/000-init-env.el
@@ -1,6 +1,7 @@
 (setenv "PAGER"      "/bin/cat")
 (setenv "CDPATH"     ".:~/projects")
 (setenv "RBENV_ROOT" "/opt/rbenv")
+(setenv "EDITOR"     "~/bin/editor")
 (setenv "PATH"       (concat "~/bin:"
                              (getenv "RBENV_ROOT") "/bin:"
                              (getenv "RBENV_ROOT") "/shims:"
