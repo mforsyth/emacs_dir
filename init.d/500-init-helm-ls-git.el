806cac7593637b4e20fba71e3f8cad583b01e6c5
David Leal <dgleal@gmail.com>
2013-03-24 19:24:16 +0000

    Move helm-ls-git customizations to init-helm.

--- init.d/500-init-helm-ls-git.el
+++
@@ -1,8 +0,0 @@
-(require 'helm-ls-git)
-
-(setq helm-ls-git-show-abs-or-relative 'relative)
-
-(defun helm-in-default-git-project ()
-  (interactive)
-  (let ((default-directory command-line-default-directory))
-    (helm-ls-git-ls)))
--- init.d/500-init-helm.el
+++ init.d/500-init-helm.el
@@ -1,7 +1,15 @@
 (require 'helm-config)
 (require 'helm-match-plugin)
+(require 'helm-ls-git)
+
+(setq helm-ls-git-show-abs-or-relative 'relative)
 
 (setq helm-buffer-max-length 55)
 (setq helm-ff-transformer-show-only-basename nil)
 
 (helm-mode 1)
+
+(defun helm-in-default-git-project ()
+  (interactive)
+  (let ((default-directory command-line-default-directory))
+    (helm-ls-git-ls)))
