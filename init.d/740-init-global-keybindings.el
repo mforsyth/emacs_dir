7dcb317944042e7197de903dcf545d173abe3925
David Leal <dgleal@gmail.com>
2013-03-24 23:18:16 +0000

    Reorder global keybindings.

--- init.d/740-init-global-keybindings.el
+++ init.d/740-init-global-keybindings.el
@@ -2,15 +2,17 @@
 (global-set-key "\M-x" 'helm-M-x)
 (global-set-key "\C-xb" 'helm-with-project)
 (global-set-key "\C-x\C-b" 'helm-buffers-list)
+
 (global-set-key "\M-i" 'indent-relative)
-(global-set-key "\C-cf" 'vc-git-grep)
-(global-set-key "\C-cg" 'magit-status)
-(global-set-key "\C-cc" 'display-compilation)
-
-(global-set-key "\C-cs" 'default-term)
-
 (global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key "\M-y" 'helm-show-kill-ring)
+
+(global-set-key "\C-cf" 'vc-git-grep)
+(global-set-key "\C-cg" 'magit-status)
+(global-set-key "\C-cc" 'display-console)
+(global-set-key "\C-cp" 'display-compilation)
+(global-set-key "\C-cs" 'default-term)
+
 (global-set-key "\C-ct" 'touch)
 
 (global-set-key "\M-^" 'project/async-shell-command)
