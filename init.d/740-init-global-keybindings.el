afad965916e476b89319abd0258ef604ad078eed
David Leal <dgleal@gmail.com>
2013-03-24 19:01:55 +0000

    Change keybinding for git grep.

--- init.d/740-init-global-keybindings.el
+++ init.d/740-init-global-keybindings.el
@@ -3,7 +3,7 @@
 (global-set-key "\C-xb" 'helm-mini)
 (global-set-key "\C-x\C-b" 'helm-buffers-list)
 (global-set-key "\M-i" 'indent-relative)
-(global-set-key "\C-cvg" 'vc-git-grep)
+(global-set-key "\C-cf" 'vc-git-grep)
 (global-set-key "\C-cg" 'magit-status)
 
 (global-set-key "\C-cs" 'default-term)
