9990c45790a15c74fa47ad9ff81ed78672cff3be
David Leal <dgleal@gmail.com>
2013-03-24 18:50:14 +0000

    Add $HOME/bin to $PATH.

--- init.d/000-init-env.el
+++ init.d/000-init-env.el
@@ -1,4 +1,7 @@
 (setenv "PAGER"      "/bin/cat")
 (setenv "CDPATH"     ".:~/projects")
 (setenv "RBENV_ROOT" "/opt/rbenv")
-(setenv "PATH" (concat (getenv "RBENV_ROOT") "/bin:" (getenv "RBENV_ROOT") "/shims:" (getenv "PATH")))
+(setenv "PATH"       (concat "~/bin:"
+                             (getenv "RBENV_ROOT") "/bin:"
+                             (getenv "RBENV_ROOT") "/shims:"
+                             (getenv "PATH")))
