268d9adfdbfa80e929830fd1f4a816370760f990
David Leal <dgleal@gmail.com>
2013-03-25 22:59:51 +0000

    Add $LEIN_HOME while sorting env var names.

--- init.d/000-init-env.el
+++ init.d/000-init-env.el
@@ -1,8 +1,10 @@
-(setenv "PAGER"      "/bin/cat")
 (setenv "CDPATH"     ".:~/projects")
-(setenv "RBENV_ROOT" "/opt/rbenv")
 (setenv "EDITOR"     "~/bin/editor")
+(setenv "LEIN_HOME"  "/opt/lein")
+(setenv "PAGER"      "/bin/cat")
+(setenv "RBENV_ROOT" "/opt/rbenv")
 (setenv "PATH"       (concat "~/bin:"
+                             (getenv "LEIN_HOME")  "/bin:"
                              (getenv "RBENV_ROOT") "/bin:"
                              (getenv "RBENV_ROOT") "/shims:"
                              (getenv "PATH")))
