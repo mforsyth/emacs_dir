04890ebbbd992bcc70ea2b6b6776b95ebda33354
David Leal <dgleal@gmail.com>
2013-03-27 19:17:06 +0000

    Add missing requires to have nrepl work.

--- init.d/500-init-clojure.el
+++ init.d/500-init-clojure.el
@@ -1,4 +1,6 @@
 (require 'nrepl)
+(require 'paredit)
+(require 'rainbow-delimiters)
 
 (setq nrepl-hide-special-buffers t)
 (setq nrepl-popup-stacktraces nil)
@@ -7,7 +9,7 @@
 (add-hook 'clojure-mode-hook 'paredit-mode)
 
 (add-hook 'nrepl-mode-hook (lambda () (autopair-mode -1)))
-; (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
 (add-hook 'nrepl-mode-hook 'subword-mode)
 (add-hook 'nrepl-mode-hook 'paredit-mode)
 (add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
