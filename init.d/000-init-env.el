cbc8e73e192d4ea892b07ba3006ec30b16570885
David Leal <dgleal@gmail.com>
2013-04-21 10:23:13 +0100

    Stop using some environment variables.

--- init.d/000-init-env.el
+++ init.d/000-init-env.el
@@ -1,10 +1,3 @@
 (setenv "CDPATH"     ".:~/projects")
 (setenv "EDITOR"     "~/bin/editor")
-(setenv "LEIN_HOME"  "/opt/lein")
 (setenv "PAGER"      "/bin/cat")
-(setenv "RBENV_ROOT" "/opt/rbenv")
-(setenv "PATH"       (concat "~/bin:"
-                             (getenv "LEIN_HOME")  "/bin:"
-                             (getenv "RBENV_ROOT") "/bin:"
-                             (getenv "RBENV_ROOT") "/shims:"
-                             (getenv "PATH")))
