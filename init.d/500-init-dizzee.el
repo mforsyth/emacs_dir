ca96c32dfbf94656f3d75d4fd48c8a3ae84f60b2
David Leal <dgleal@gmail.com>
2013-08-16 15:54:30 +0100

    [dizzee] Enable starting zeus.

--- init.d/500-init-dizzee.el
+++ init.d/500-init-dizzee.el
@@ -1,5 +1,9 @@
 (require 'dizzee)
 
+(dz-defservice zeus "zeus-wrapper"
+               :args ("start")
+               :cd command-line-default-directory)
+
 (dz-defservice spork-cucumber "spork"
                :args ("cucumber")
                :cd command-line-default-directory)
