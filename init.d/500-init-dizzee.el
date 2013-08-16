6d4fc6bd0294c27c81baf25e38d13073a020226f
David Leal <dgleal@gmail.com>
2013-08-16 15:54:51 +0100

    [dizzee] Enable starting guard.

--- init.d/500-init-dizzee.el
+++ init.d/500-init-dizzee.el
@@ -1,5 +1,8 @@
 (require 'dizzee)
 
+(dz-defservice guard "guard"
+               :cd command-line-default-directory)
+
 (dz-defservice zeus "zeus-wrapper"
                :args ("start")
                :cd command-line-default-directory)
