40fedc7d7f7f4f32933388a365bb9a4280a9766f
David Leal <dgleal@gmail.com>
2013-08-16 15:55:03 +0100

    [dizzee] Remove spork.

--- init.d/500-init-dizzee.el
+++ init.d/500-init-dizzee.el
@@ -6,11 +6,3 @@
 (dz-defservice zeus "zeus-wrapper"
                :args ("start")
                :cd command-line-default-directory)
-
-(dz-defservice spork-cucumber "spork"
-               :args ("cucumber")
-               :cd command-line-default-directory)
-
-(dz-defservice spork-rspec "spork"
-               :args ("rspec")
-               :cd command-line-default-directory)
