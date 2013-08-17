bf6226471cc35dd0bbfcabfd98b368f8d9d32143
David Leal <dgleal@gmail.com>
2013-08-17 13:00:55 +0100

    [magit] Rewrite magit-setup for clarity.

--- init.d/500-init-magit.el
+++ init.d/500-init-magit.el
@@ -19,16 +19,30 @@
 
 (add-to-list 'display-buffer-alist
              '("^\\*magit-edit-log\\*$" . (display-buffer-pop-up-window . nil)))
+(add-to-list 'display-buffer-alist
+             '("^\\*magit:" . (display-buffer-same-window . nil)))
 
-(defun magit-setup ()
+(defun magit/browser ()
   (interactive)
-  (let ((current (current-buffer))
-        (bcommit (get-buffer-create "*magit-commit*"))
-        (blog (get-buffer-create "*magit-log*")))
-    (display-buffer-other-frame bcommit)
-    (display-buffer-other-frame blog)
-    (with-current-buffer blog (magit-log))
-    (with-current-buffer current (magit-status "."))))
+  (magit/make-status-frame)
+  (magit/make-log-frame)
+  (magit/make-commit-frame))
+
+(defun magit/make-status-frame ()
+  (select-frame (make-frame))
+  (magit-status "."))
+
+(defun magit/make-log-frame ()
+  (magit/-make-frame-with-buffer "*magit-log*")
+  (magit-log))
+
+(defun magit/make-commit-frame ()
+  (magit/-make-frame-with-buffer "*magit-commit*"))
+
+(defun magit/-make-frame-with-buffer (name)
+  (select-frame (make-frame))
+  (display-buffer (get-buffer-create name)
+                  '(display-buffer-same-window . ())))
 
 (add-hook 'magit-log-edit-mode-hook
           (lambda () (setq fill-column 70)))
