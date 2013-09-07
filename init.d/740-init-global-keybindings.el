71e4b024576a93d7d2f0788d9c84eca5597c3036
David Leal <dgleal@gmail.com>
2013-09-07 18:40:47 +0100

    [keybindings] Add some keybindings.

--- init.d/740-init-global-keybindings.el
+++ init.d/740-init-global-keybindings.el
@@ -2,7 +2,10 @@
 (global-set-key "\M-x" 'helm-M-x)
 (global-set-key "\C-xb" 'helm-with-project)
 (global-set-key "\C-x\C-b" 'helm-buffers-list)
+(global-set-key "\M-." 'helm-etags-select)
+(global-set-key "\M-," 'helm-imenu)
 
+(global-set-key"\C-xw" 'kill-region)
 (global-set-key "\M-i" 'indent-relative)
 (global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key "\M-y" 'helm-show-kill-ring)
@@ -12,9 +15,10 @@
 (global-set-key "\C-cp" 'display-compilation)
 (global-set-key "\C-cs" 'eshell)
 (global-set-key "\C-ck" 'display-clock)
+(global-set-key "\C-w" 'backward-kill-word)
 
 (global-set-key "\C-ct" 'touch)
 
-(global-set-key "\M-^" 'project/async-shell-command)
+(global-set-key "\M-&" 'project/async-shell-command)
 
 (global-set-key "\C-x\C-c" 'save-buffers-kill-emacs)
