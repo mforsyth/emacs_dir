0cb2fb798d258c37d1d2ae0fa1f42a2d34bb5927
David Leal <dgleal@gmail.com>
2013-09-07 18:22:33 +0100

    [evil] Remove evil.

--- init.d/500-init-ace-jump-mode.el
+++ init.d/500-init-ace-jump-mode.el
@@ -2,7 +2,4 @@
 
 (setq ace-jump-mode-case-fold nil)
 
-(dolist (mode '(evil-normal-state-map evil-visual-state-map evil-operator-state-map))
-  (define-key (symbol-value mode) (kbd "SPC") 'evil-ace-jump-word-mode)
-  (define-key (symbol-value mode) (kbd "C-SPC") 'evil-ace-jump-char-mode)
-  (define-key (symbol-value mode) (kbd "S-SPC") 'evil-ace-jump-word-mode))
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
--- init.d/500-init-magit.el
+++ init.d/500-init-magit.el
@@ -3,20 +3,6 @@
 (setq magit-revert-item-confirm t)
 (setq magit-repo-dirs '("~/projects/"))
 
-(dolist (mode
-         (list magit-log-mode-map
-               magit-status-mode-map
-               magit-commit-mode-map
-               magit-diff-mode-map
-               magit-branch-manager-mode-map))
-  (define-key mode "j" 'magit-goto-next-section)
-  (define-key mode "k" 'magit-goto-previous-section)
-  (define-key mode "/" 'evil-search-forward)
-  (define-key mode "?" 'evil-search-backward)
-  (define-key mode "n" 'evil-search-next)
-  (define-key mode "N" 'evil-search-previous)
-  (define-key mode "d" 'magit-discard-item))
-
 (add-to-list 'display-buffer-alist
              '("^\\*magit-edit-log\\*$" . (display-buffer-pop-up-window . nil)))
 (add-to-list 'display-buffer-alist
