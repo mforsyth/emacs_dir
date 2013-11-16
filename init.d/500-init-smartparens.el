094b1a53be4b7174773ad7696a2ba88ee06a4f94
David Leal <dgleal@gmail.com>
2013-11-16 11:53:35 +0000

    Add a few more packages.

--- init.d/500-init-smartparens.el
+++ init.d/500-init-smartparens.el
@@ -1,4 +1,7 @@
 (require 'smartparens-config)
 (require 'smartparens-ruby)
+
+(sp-use-smartparens-bindings)
 (smartparens-global-mode)
+(smartparens-global-strict-mode)
 (show-smartparens-global-mode t)
