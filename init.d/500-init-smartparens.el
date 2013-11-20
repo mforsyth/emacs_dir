4d8bde5c6156340e8fdb5de900c3f9a1cfa9279c
David Leal <dgleal@gmail.com>
2013-11-20 23:17:00 +0000

    Tone down smartparens a bit.

--- init.d/500-init-smartparens.el
+++ init.d/500-init-smartparens.el
@@ -1,7 +1,5 @@
 (require 'smartparens-config)
 (require 'smartparens-ruby)
 
-(sp-use-smartparens-bindings)
 (smartparens-global-mode)
-(smartparens-global-strict-mode)
 (show-smartparens-global-mode t)
