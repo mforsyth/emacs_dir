42a5ee853e635ea76754c1e33dd681710fd68471
David Leal <dgleal@gmail.com>
2014-01-09 18:45:19 +0000

    [coffee] Set explicit tab width.

--- init.d/500-coffee-mode.el
+++ init.d/500-coffee-mode.el
@@ -3,6 +3,8 @@
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
+(setq coffee-tab-width 2)
+
 (add-hook
  'coffee-mode-hook
  (lambda ()
