dfcfeace0b37e2516716451569795c1a316f7af6
David Leal <dgleal@gmail.com>
2014-01-09 18:48:46 +0000

    [clojure] Add more configuration.

--- init.d/500-init-clojure.el
+++ init.d/500-init-clojure.el
@@ -1,2 +1,8 @@
 (require 'clojure-mode)
 (require 'cider)
+
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+(dolist (hook '(clojure-mode-hook cider-repl-mode-hook))
+  (add-hook hook 'subword-mode)
+  (add-hook hook 'rainbow-delimiters-mode))
