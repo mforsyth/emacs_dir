c94609faf9c1a8f15d99e1a82402bec27b70d7f0
David Leal <dgleal@gmail.com>
2014-11-01 19:14:12 +0000

    Remove flymake stuff.

--- init.d/500-ruby-mode.el
+++ init.d/500-ruby-mode.el
@@ -16,21 +16,6 @@
 (add-to-list 'special-display-buffer-names "*rake*")
 (add-to-list 'same-window-buffer-names "*ruby*")
 
-;;; Flymake
-(defun flymake-ruby-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-	 (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "ruby" (list "-w" "-W2" "-c" local-file))))
-
-(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("Thorfile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("Gemfile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
-
 ;;; Compilation
 (add-to-list
  'compilation-error-regexp-alist
