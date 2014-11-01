11f10ed3b45fb7433fecc2870046fd2cea2a5d95
David Leal <dgleal@gmail.com>
2014-11-01 19:15:42 +0000

    Move feature mode to config.org.

--- init.d/500-feature-mode.el
+++
@@ -1,8 +0,0 @@
-(require 'feature-mode)
-
-(setq feature-cucumber-command "bin/cucumber -rfeatures -P -f pretty {options} {feature}")
-
-(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
-
-(add-hook 'feature-mode-hook 'auto-fill-mode)
-(add-hook 'feature-mode-hook 'fci-mode)
