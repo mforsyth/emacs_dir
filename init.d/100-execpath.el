14d76c767816412aa99b3adbf7db9d76053e9414
Matthew Forsyth <matt@mojotech.com>
2015-01-12 15:39:40 -0500

    Add more to the executable paths

---
+++ init.d/100-execpath.el
@@ -0,0 +1,4 @@
+(setq exec-path (append exec-path '("/usr/local/bin")))
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/Users/andalucien/bin")))
+(setenv "PATH" (concat (getenv "PATH") ":/Users/andalucien/bin"))
